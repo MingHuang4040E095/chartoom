9b7c3f2cd08c43bc1abfa33fe6bad64fe74250f0
MingHuang1220 <syuminghuang@gmail.com>
2023-11-28 21:30:55 +0800

    建立路由&首頁

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard', 'plugin:vue/vue3-essential'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    indent: ['error', 2], // tab
+    'linebreak-style': ['off', 'windows'], // ['off', 'windows'] 或者 ['error', 'windows']
+    quotes: ['error', 'single'], // 使用單引號 ' '
+    semi: [2, 'never'], // 句尾去分號
+    'no-extra-semi': 'error',
+    'comma-dangle': ['error', 'always-multiline'], // 自動補逗號
+    'no-unused-vars': ['error', { vars: 'all' }], // 禁止未使用過的變量
+    'prefer-const': 'error', // 沒有重複使用 就要用const
+    'no-var': 'error', // 不使用var
+  },
+}
---
+++ src/views/TheHome.vue
@@ -0,0 +1,6 @@
+<script setup>
+
+</script>
+<template>
+    <div>I'm home page</div>
+</template>
