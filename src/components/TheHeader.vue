fad6423a4e89a39db1bb6ab675c1215f2b1f2e2c
MingHuang1220 <syuminghuang@gmail.com>
2024-05-25 21:22:00 +0800

    header組件

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+<template>
+  <header
+    class="flex justify-between items-center bg-secondary-200 w-full px-10 py-4"
+  >
+    <div class="logo text-primary-200 font-size-6">logo</div>
+    <nav class="flex gap-4 text-primary-200 font-size-4 [&>div]:cursor-pointer">
+      <div>message</div>
+      <div>userName</div>
+      <div>setting</div>
+      <div>登入</div>
+      <div>註冊</div>
+    </nav>
+  </header>
+</template>
