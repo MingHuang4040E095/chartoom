e34f6e09e7bef575a10d1ead3389ea2248077ba6
MingHuang1220 <syuminghuang@gmail.com>
2024-05-25 21:22:15 +0800

    套用房間卡片及header組件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import TheHeader from '@/components/TheHeader.vue'
 </script>
 <template>
+  <TheHeader />
   <RouterView></RouterView>
 </template>
 <style lang="scss">
-body{
-  background-color: #EEE4E1;
+body {
+  background-color: #eee4e1;
 }
 </style>
--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import RoomCard from '@/components/RoomCard.vue'
+</script>
 <template>
   <section>
     <container>
@@ -12,40 +14,10 @@
                     </div>
                 </form>
             </aside> -->
-      <header
-        class="flex justify-between items-center bg-secondary-200 w-full px-10 py-4"
-      >
-        <div class="logo text-primary-200 font-size-6">logo</div>
-        <nav
-          class="flex gap-4 text-primary-200 font-size-4 [&>div]:cursor-pointer"
-        >
-          <div>message</div>
-          <div>userName</div>
-          <div>setting</div>
-          <div>登入</div>
-          <div>註冊</div>
-        </nav>
-      </header>
       <body
         class="p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8"
       >
-        <div
-          v-for="i in 16"
-          :key="i"
-          class="card flex flex-col bg-secondary-200 rounded-1 aspect-ratio-video tracking-widest"
-        >
-          <h2 class="text-primary-100 px-6">聊天室1</h2>
-          <div class="text-primary-200 font-size-4 px-6">
-            我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容
-          </div>
-          <div class="text-right mt-auto px-6 py-4">
-            <button
-              class="text-white bg-primary-100 rounded-8 border-none px-4 py-2 tracking-widest cursor-pointer hover:bg-primary-200 transition-all duration-300 ease-in-out"
-            >
-              加入
-            </button>
-          </div>
-        </div>
+        <RoomCard v-for="i in 16" :key="i" />
       </body>
     </container>
   </section>
