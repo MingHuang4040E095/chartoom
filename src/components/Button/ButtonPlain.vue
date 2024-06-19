f49f8fb92a364f2c67f9b5610ad40ef780b7bcdb
MingHuang1220 <syuminghuang@gmail.com>
2024-06-19 20:43:57 +0800

    新增邊框按鈕組件

---
+++ src/components/Button/ButtonPlain.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+  <button
+    class="text-primary-100 bg-transparent font-size-4 border-2px border-solid border-primary-100 rounded-1 px-4 py-2 tracking-widest cursor-pointer hover:bg-primary-100 hover:text-white transition-all duration-300 ease-in-out w-full"
+  >
+    <slot></slot>
+  </button>
+</template>
