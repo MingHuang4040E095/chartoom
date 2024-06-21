f6326170447cd9de4d54c3ca258718ba9a999a83
MingHuang1220 <syuminghuang@gmail.com>
2024-06-21 23:58:41 +0800

    補上說明註解

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,4 +1,15 @@
-<script setup></script>
+<script setup>
+// 分頁
+/**
+ * 1.如果中間頁數低等於5頁，則不用顯示箭頭
+ * 中間頁數: 去掉頭尾頁數後的頁數
+ * ex: 1  2  3  全部3頁，中間頁數只有1頁(2)
+ * ex: 1  2  3  4  5  6 全部6頁，中間頁數只有3頁(2 3 4 5)
+ * ex: 1  2  3  4  5  6 7 全部7頁，中間頁數只有5頁(2 3 4 5 6)
+ *
+ * 2.如果中間頁數高於5頁，則顯示箭頭
+ */
+</script>
 <template>
   <div class="pagination">
     <button class="pagination__btn">
