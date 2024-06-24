0bd83b2ef28d5681d90a7ed98ee2067941ff445a
MingHuang1220 <syuminghuang@gmail.com>
2024-06-24 22:01:32 +0800

    dialog基礎架構

---
+++ src/components/Feedback/Dialog.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineModel, defineProps } from 'vue'
+const modelValue = defineModel({
+  default: true,
+  type: Boolean,
+})
+
+const props = defineProps({
+  // 標題
+  title: {
+    type: String,
+    default: '標題',
+  },
+})
+</script>
+<template>
+  <div
+    class="warp bg-#0000007a w-full h-full fixed top-0 left-0 flex justify-center items-center"
+  >
+    <div
+      v-if="modelValue"
+      class="dialog bg-white w-full max-w-xl h-lg rounded-2 z-2000"
+    >
+      <div>{{ props.title }}</div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
