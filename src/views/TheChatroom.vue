ed5cf19a7b762107bed12ed0a2f8f05c9573a776
MingHuang1220 <syuminghuang@gmail.com>
2024-07-05 22:35:28 +0800

    聊天室人員列表

--- src/views/TheChatroom.vue
+++ src/views/TheChatroom.vue
@@ -1,17 +1,31 @@
 <script setup>
+import ButtonBasic from '@/components/Button/ButtonBasic.vue' // 基礎按鈕樣式
 import { ref } from 'vue'
 
 const inputValue = ref('')
 </script>
 <template>
   <section class="flex">
-    <div class="pa-4">
+    <div class="pa-4 w-full max-w-300px">
       <aside
-        class="the-aside bg-secondary-200 max-w-lg h-full w-full p-10 rounded-2"
+        class="the-aside flex flex-col bg-secondary-200 h-full px-10 py-4 rounded-2"
       >
-        <h1 class="text-primary-200 font-size-12 font-bold text-center">
+        <h3 class="text-primary-200 font-size-9 font-bold text-center my-4">
           MH聊天室
-        </h1>
+        </h3>
+
+        <ul class="list-none pl-0">
+          <li v-for="i in 10" class="flex items-center gap-2 font-size-4 mb-2">
+            <span
+              class="bg-white rounded-full w-24px h-24px inline-block"
+            ></span>
+            <span>username</span>
+          </li>
+        </ul>
+
+        <ButtonBasic class="font-bold w-full mt-auto" type="round"
+          >離開</ButtonBasic
+        >
       </aside>
     </div>
     <div class="pa-4 flex flex-col flex-grow flex-shrink gap-4">
